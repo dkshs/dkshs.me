78638d58ea2eb9c84468182f20df8547be7fe8ad
DKSHS <88355080+ShadowsS01@users.noreply.github.com>
2023-08-15 18:32:27 -0300

    feat: adding font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Merriweather_Sans as MerriweatherSans } from "next/font/google";
+
+const merriweatherSans = MerriweatherSans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={merriweatherSans.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
