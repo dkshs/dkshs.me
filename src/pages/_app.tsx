63973f82fb57e9a42215e7b505aa7db1e28e3ce5
DKSHS <88355080+ShadowsS01@users.noreply.github.com>
2023-08-18 13:02:35 -0300

    update: body styles and background

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ const merriweatherSans = MerriweatherSans({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={merriweatherSans.className}>
+      <div className="fixed inset-0 -z-[1] bg-black bg-gradient-to-b from-black/60 to-violet-900/20" />
       <Component {...pageProps} />
     </div>
   );
