99836f233d9789c369c8fbcf3857f998f4d79a02
dkshs <88355080+dkshs@users.noreply.github.com>
2023-11-23 18:47:40 -0300

    feat: removing icons in favor of `icon.tsx`

---
+++ src/app/icon.tsx
@@ -0,0 +1,22 @@
+import { ImageResponse } from "next/og";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div tw="flex w-full h-full items-center justify-center bg-black rounded-full" />
+    ),
+    { ...size },
+  );
+}
