523ccf7bbae2416cecbacfdc0e784377413c8a69
Nicolas Contiero <88355080+dkshs@users.noreply.github.com>
2025-01-21 22:22:48 -0300

    fix: update "demo" to "site"

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -95,7 +95,7 @@ export default async function ProjectPage(props: Props) {
               rel="noopener noreferrer"
               className="group flex items-center space-x-2"
             >
-              Demo
+              Site
               <ArrowUpRight
                 size={20}
                 className="duration-300 group-hover:-translate-y-0.5 group-hover:translate-x-0.5"
