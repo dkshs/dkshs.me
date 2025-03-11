36fe7319f36d97b06d72f132558d8d4e5b3ca745
dkshs <88355080+dkshs@users.noreply.github.com>
2025-03-11 20:35:51 -0300

    chore: update page error component

--- src/components/PageError.tsx
+++ src/components/PageError.tsx
@@ -8,7 +8,7 @@ interface PageErrorProps {
 
 export function PageError({ title, description }: PageErrorProps) {
   return (
-    <main className="flex h-screen flex-col items-center justify-center text-center">
+    <main className="flex flex-col items-center justify-center text-center">
       <h1 className="text-4xl font-bold tracking-wide">{title}</h1>
       <p className="mt-4 text-lg">{description}</p>
       <p className="mt-2 text-base">
