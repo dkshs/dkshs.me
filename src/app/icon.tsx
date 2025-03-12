e4efd74b8ac93a825b449cdf2394a920ca312d1a
dkshs <88355080+dkshs@users.noreply.github.com>
2025-03-11 21:03:56 -0300

    chore: update icon

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -2,19 +2,15 @@
 /* eslint-disable react/no-unknown-property */
 import { ImageResponse } from "next/og";
 
-// Image metadata
-export const size = {
-  width: 32,
-  height: 32,
-};
+export const size = { width: 52, height: 52 };
 export const contentType = "image/png";
 
-// Image generation
 export default function Icon() {
   return new ImageResponse(
     (
-      // ImageResponse JSX element
-      <div tw="flex h-full w-full items-center justify-center rounded-full bg-black" />
+      <div tw="flex h-full w-full items-center justify-center rounded-full bg-black text-3xl font-bold text-white">
+        DK
+      </div>
     ),
     { ...size },
   );
