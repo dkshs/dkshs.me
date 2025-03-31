a63f6287d7bff14427234cb959eaf97f9258c3f7
Nicolas Contiero <git@ncontiero.com>
2025-03-30 23:48:57 -0300

    feat: display initials in app icon

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,15 +1,20 @@
 /* eslint-disable tailwindcss/enforces-shorthand */
 /* eslint-disable react/no-unknown-property */
 import { ImageResponse } from "next/og";
+import { data } from "@/data";
 
 export const size = { width: 52, height: 52 };
 export const contentType = "image/png";
 
 export default function Icon() {
+  const splittedName = data.name.split(" ");
+  const firstNameLetter = splittedName[0]![0];
+  const lastNameLetter = splittedName[1]![0];
+
   return new ImageResponse(
     (
       <div tw="flex h-full w-full items-center justify-center rounded-full bg-black text-3xl font-bold text-white">
-        DK
+        {`${firstNameLetter}${lastNameLetter}`}
       </div>
     ),
     { ...size },
